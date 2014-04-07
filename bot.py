afe916a767e9fca3bbd5ab1bf4550562a725a823
TurpIF <pierre.turpin@insa-lyon.fr>
2014-04-07 23:47:49 +0200

    Ajout d'un lock dans la classe Point directement

--- bot.py
+++ bot.py
@@ -20,54 +20,43 @@ class Point(object):
         self.x = x
         self.y = y
         self.a = a
+        self.lock = Lock()
 
     def get(self):
         """
-        Return a tuple (x, y, a) of the current position.
+        Return a tuple (x, y, a) of the current position. The call is
+        thread-safe due to an internal lock.
 
         Returns:
         Tuple of the position
         """
-        return (self.x, self.y, self.a)
+        with self.lock:
+            return (self.x, self.y, self.a)
 
     def set(self, x, y, a):
         """
-        Set entirely the point.
+        Set entirely the point. The call is thread-safe due to an internal
+        lock.
 
         Arguments:
         x -- Real : Position on the X axis
         y -- Real : Position on the Y axis
         a -- Radian : Angular position in direct sens
         """
-        self.x = x
-        self.y = y
-        self.a = a
-
-
-class PointProxy(BaseProxy):
-    _exposed_ = ['get', 'set']
-
-    def __init__(self):
-        super(PointProxy, self).__init__()
-        self.lock = Lock()
-
-    def get(self):
-        return self
-
-    def set(self, x, y, a):
         with self.lock:
-            self._callmethod('set', (x, y, a))
+            self.x = x
+            self.y = y
+            self.a = a
 
 
-order_pos = Point(0, 0)
-real_pos = Point(0, 0)
-
+order_pos = Point(0, 0, 0)
+real_pos = Point(0, 0, 0)
 
 class BotManager(BaseManager):
     pass
 
-BotManager.register('order_pos', lambda: order_pos, proxytype=PointProxy)
-BotManager.register('real_pos', lambda: real_pos, proxytype=PointProxy)
+BotManager.register('order_pos', lambda: order_pos, exposed=['get', 'set'])
+BotManager.register('real_pos', lambda: real_pos, exposed=['get', 'set'])
 
 if __name__ == '__main__':
     manager = BotManager(address='./bot.sock', authkey=bytes('42', 'ascii'))
